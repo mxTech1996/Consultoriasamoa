f84d23ae551e60212489829d9ea74ad24e963067
juliotwo <juliotwo1996@gmail.com>
2025-02-18 19:12:42 -0600

    fix

--- src/app/my-cart/page.js
+++ src/app/my-cart/page.js
@@ -2,25 +2,13 @@
 import CartSectionComponent from '@/components/organisms/CartSection';
 import Footer from '@/components/organisms/Footer';
 import Navbar from '@/components/organisms/Navbar';
-import { useEffect } from 'react';
 
 export default function MyCart() {
-  useEffect(() => {
-    document.querySelectorAll('*').forEach((element) => {
-      element.setAttribute('translate', 'no');
-    });
-  }, []);
   return (
-    <>
-      <head>
-        <meta http-equiv='Content-Language' content='es' />
-        <meta name='google' content='notranslate' />
-      </head>
-      <main>
-        <Navbar />
-        <CartSectionComponent />
-        <Footer />
-      </main>
-    </>
+    <main>
+      <Navbar />
+      <CartSectionComponent />
+      <Footer />
+    </main>
   );
 }
